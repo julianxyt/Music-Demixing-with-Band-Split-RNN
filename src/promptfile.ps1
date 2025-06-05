e76ac40f6c6d9db5d1d66df9bc07d2dddfe65633
Julian Tang <julianxytang@gmail.com>
2025-06-06 00:46:02 +1000

    a

--- src/promptfile.ps1
+++ src/promptfile.ps1
@@ -1,16 +1,17 @@
-$files = ["D:\Music - HDD\Music Repository\2025 Club\Girl, so confusing.mp3",
-"D:\Music - HDD\Music Repository\2025 Club\Teenage Dream (Lyrics).mp3",
-"D:\Music - HDD\Music Repository\2025 Club\Call On Me (Radio Edit) (HD).mp3",
-"D:\Music - HDD\Music Repository\2025 Club\Simple And Clean (Planitb Remix).mp3",
-"D:\Music - HDD\Music Repository\2025 Club\OMG (Audio).mp3",
-"D:\Music - HDD\Music Repository\2025 Club\PARTY 4 U.mp3",
-"D:\Music - HDD\Music Repository\2025 Club\Ma boy (MA BOY).mp3",
-"D:\Music - HDD\Music Repository\2025 Club\Perfect Night.mp3",
-"D:\Music - HDD\Music Repository\2025 Club\Clarity (feat. Foxes).mp3",
-"D:\Music - HDD\Music Repository\2025 Club\Apple.mp3"]
+$files = @(
+    "D:\Music - HDD\Music Repository\2025 Club\Girl, so confusing.mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\Teenage Dream (Lyrics).mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\Call On Me (Radio Edit) (HD).mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\OMG (Audio).mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\PARTY 4 U.mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\Ma boy (MA BOY).mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\Perfect Night.mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\Clarity (feat. Foxes).mp3",
+    "D:\Music - HDD\Music Repository\2025 Club\Apple.mp3"
+)
 # $file_name = "Satellite (Update).mp3"
 # $input = "D:/Music - HDD/Music Repository/2025 Club/$file_name"
-For ($file in $files) {
+Foreach ($file in $files) {
     $dest_folder = $file.split('.')[0]
     $output = "D:/Music - HDD/Music Repository/Spleeter/$dest_folder"
     python inference.py -i $file -o $output -t vocals -c 'C:\Users\gamer\GitHub\Music-Demixing-with-Band-Split-RNN\src\saved_models\vocals.ckpt'
